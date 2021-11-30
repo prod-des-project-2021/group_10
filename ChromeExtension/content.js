806125f2cc4db9a29dfef2e8d53573dccfba53c8
Ville Kovalainen <lolwest@suomi24.fi>
2021-11-30 13:31:15 +0200

    very basic cropping and text recog

--- ChromeExtension/content.js
+++ ChromeExtension/content.js
@@ -1,18 +1,55 @@
-
-const imageToText = () => { 
-    chrome.runtime.sendMessage({type: "notification_start"});
-    Tesseract.recognize( 'https://tesseract.projectnaptha.com/img/eng_bw.png', 'eng',
+const imageToText = (url) => { 
+    chrome.runtime.sendMessage({command: "notification_start"});
+    Tesseract.recognize( url, 'eng',
     { logger: m => {
         console.log(m.progress) 
     }}
         ).then(({ data: { text } }) => {  
-        chrome.runtime.sendMessage({type: "notification_finish"});
+        chrome.runtime.sendMessage({command: "notification_finish"});
         navigator.clipboard.writeText(text);  
-    }) 
+    });
 };
 
 chrome.runtime.onMessage.addListener((msg, sender, response) => { 
+    console.log(msg)
     if (msg.command == "recognize") {
-        imageToText();
-    }
+        imageToText(msg.src);
+    };
+
+    if (msg.command == "crop") { 
+        let cropping = true
+        let mouseCoordinates = {
+            mousedownX : 0,
+            mousedownY : 0,
+            mouseupX : 0,
+            mouseupY : 0
+        };
+         
+        document.body.onmousedown = (e) => {
+            if (cropping) {
+                mouseCoordinates.mousedownX = e.clientX;
+                mouseCoordinates.mousedownY = e.clientY;
+                console.log("mouse down");
+                console.log(e);
+            }
+        };
+
+        document.body.onmouseup = (e) => {
+            if (cropping) {
+                mouseCoordinates.mouseupX = e.clientX;
+                mouseCoordinates.mouseupY = e.clientY; 
+                console.log("mouse up");
+                console.log(e);
+                console.log(mouseCoordinates);
+                chrome.runtime.sendMessage({command:"mousecoordinates", mouseCoordinates});
+                cropping = false;
+            };
+        };
+    };
 });
+
+
+
+ 
+
+        
--- ChromeExtension/popup.js
+++ ChromeExtension/popup.js
@@ -1,26 +1,81 @@
 document.addEventListener('DOMContentLoaded', function() {
     const openPicBtn = document.getElementById('openPic');
-    const takePicBtn = document.getElementById('takePic');
-    
-    openPicBtn.addEventListener('click', function() { 
-        chrome.tabs.captureVisibleTab(null, {}, function(dataUrl) {
-            const inputImage = new Image();
-            inputImage.src = dataUrl; 
-            let img = document.createElement('img');
-            img.src = dataUrl;
-            document.body.appendChild(img);
-            
-        })
+    const cropButton = document.getElementById('cropButton');
+    let mouseCoordinates;
+
+    chrome.runtime.onMessage.addListener((request, sender, sendResponse) => { 
+        console.log("popjs listener", mouseCoordinates) 
+        mouseCoordinates = request.mouseCoordinates
     });
 
-    takePicBtn.addEventListener('click', function() {
+    cropButton.addEventListener('click', function() { 
+        
         chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
             let activeTab = tabs[0];
-            chrome.tabs.sendMessage(activeTab.id, {command: "recognize"})
+            chrome.tabs.sendMessage(activeTab.id, {command: "crop"})
+
+            chrome.tabs.captureVisibleTab(null, {}, function(dataUrl) { 
+
+                let canvas = document.createElement('canvas');
+                let screenshot = new Image();
+                screenshot.setAttribute("id", "imageID")
+                screenshot.src = dataUrl;
+                screenshot.onload = () => {
+                    canvas.width = screenshot.width;
+                    canvas.height = screenshot.height;
+                }
+                let ctx = canvas.getContext('2d');
+
+                function waitForCoordinates() {
+                    console.log("eeeeeiiiiii")
+                    if (mouseCoordinates === undefined) {
+                        setTimeout(waitForCoordinates, 200);
+                        return;
+                    }
+                    console.log("jooooooo")
+                    let croppedWidth = mouseCoordinates.mouseupX - mouseCoordinates.mousedownX;
+                    let croppedHeight = mouseCoordinates.mouseupY - mouseCoordinates.mousedownY;
+                    console.log(croppedWidth)
+                    console.log(croppedHeight)
+
+                    ctx.drawImage(screenshot, mouseCoordinates.mousedownX, mouseCoordinates.mousedownY,
+                        croppedWidth, croppedHeight,
+                        0, 0, croppedWidth, croppedHeight);
+
+                    screenshot.src = canvas.toDataURL('image/png');
+                    console.log(screenshot.src)
+                    console.log("waiting")
+
+                    setTimeout(() => {
+                        let croppedcanvas = document.createElement('canvas');
+                        console.log(croppedcanvas)
+                        croppedcanvas.width = croppedWidth;
+                        croppedcanvas.height = croppedHeight;
+                        console.log(croppedcanvas)
+                        
+                        let croppedctx = croppedcanvas.getContext('2d');
+                        croppedctx.drawImage(screenshot, 0, 0,
+                            croppedWidth, croppedHeight,
+                            0, 0, croppedWidth, croppedHeight);
+                        console.log(croppedWidth)
+                        console.log(croppedHeight)
+    
+                        screenshot.src = croppedcanvas.toDataURL('image/png');
+                        console.log(screenshot.src)
+                        console.log(screenshot)
+
+                        document.body.appendChild(screenshot)
+                        chrome.tabs.sendMessage(activeTab.id, {command: "recognize", src: screenshot.src})
+                   },100)
+                }
+                waitForCoordinates()
+            })
         })
-        alert('takePic');
-    });  
+    })
+
+
+
+    openPicBtn.addEventListener('click', function() { 
+        alert("useless")
+    }); 
  });
-
- 
-
